c15ca61bf989839ec8ec0b3d2de68f5757af4c8a
anna <anytagolenok2@ukr.net>
2026-03-05 12:49:06 +0100

    Оновлено main.ts: додано базовий HTML для GitHub Pages

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,9 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const app = document.querySelector<HTMLDivElement>('#app')!;
+app!.innerHTML = `
+  <h1>Мої задачі TypeScript</h1>
+`;
 
 import "./task-1";
 import "./task-2";
@@ -31,3 +13,4 @@ import "./task-5";
 import "./task-6";
 import "./task-7";
 import "./task-8";
+console.log("Всі задачі завантажені!");
